16bef6841dc1ff6f2387c3384aff284f8eeb4a2e
taimtzcano <147677762+taimtzcano@users.noreply.github.com>
2023-10-12 21:29:27 -0600

    Add files via upload

---
+++ POOP1_Maven/src/main/java/com/mycompany/poop1_maven/POOP1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author Martinez Cano Tania
+ */
+public class POOP1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde POO Grupo 8 Maven");
+    }
+}
